0633f468548dce523dee4d2abd37eeea1c6444c0
Tayyab054 <2024cs491@student.uet.edu.pk>
2025-08-28 23:50:42 +0500

    c

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ function Footer() {
         <div className='bg-black text-white  px-2 py-20 lg:px-40'>
             <div className='flex'>
                 <div className="flex-1">
-                    
-                        <h2 className='font-bold py-4 text-3xl'>Tayyab Ali</h2>
-                        <p>
-                            <p className="">©{new Date().getFullYear()} Portfolio. All rights reserved.</p>
-                        </p>
+
+                    <h2 className='font-bold py-4 text-3xl'>Tayyab Ali</h2>
+                    <p>
+                        <p className="">©{new Date().getFullYear()} Portfolio. All rights reserved.</p>
+                    </p>
                 </div>
                 {/* <div className='flex-1'>
                     <form action="" className="flex flex-col sm:flex-row gap-3" >
@@ -20,22 +20,22 @@ function Footer() {
                     </div>
                  </form>
                 </div> */}
-                 <div className="flex-1">
-      <form className="flex flex-col sm:flex-row gap-3">
-        <input
-          type="email"
-          required
-          className="w-full rounded-lg p-2 bg-gray-900 text-white border border-transparent focus:outline-none focus:border-teal-400"
-          placeholder="Enter your Email"
-        />
-        <button
-          type="submit"
-          className="bg-teal-500 text-white px-4 py-2 rounded-lg hover:bg-teal-600 transition"
-        >
-          Subscribe
-        </button>
-      </form>
-    </div>
+                <div className="flex-1">
+                    <form className="flex flex-col sm:flex-row gap-3">
+                        <input
+                            type="email"
+                            required
+                            className="w-full rounded-lg p-2 bg-gray-900 text-white border border-transparent focus:outline-none focus:border-teal-400"
+                            placeholder="Enter your Email"
+                        />
+                        <button
+                            type="submit"
+                            className="bg-teal-500 text-white px-4 py-2 rounded-lg hover:bg-teal-600 transition"
+                        >
+                            Subscribe
+                        </button>
+                    </form>
+                </div>
 
             </div>
 
