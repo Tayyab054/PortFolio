7fbecb79da8d734cdec6a3875535aeb684875111
Tayyab054 <2024cs491@student.uet.edu.pk>
2025-08-29 12:25:03 +0500

    c

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,15 +11,6 @@ function Footer() {
                         <p className="">©{new Date().getFullYear()} Portfolio. All rights reserved.</p>
                     </p>
                 </div>
-                {/* <div className='flex-1'>
-                    <form action="" className="flex flex-col sm:flex-row gap-3" >
-                    <div className='flex justify-between -space-x-3.5 '>
-                        <input type="email" className='w-full rounded-lg p-2 bg-gray-900 text-white border border-transparent focus:outline-none focus:border-teal-400'
-                            placeholder='Enter your Email' />
-                        <button type="submit" className="bg-teal-500 text-white px-4 py-2 rounded-lg hover:bg-teal-600 transition" href="#Contact">Subsucribe</button>
-                    </div>
-                 </form>
-                </div> */}
                 <div className="flex-1">
                     <form className="flex flex-col sm:flex-row gap-3">
                         <input
